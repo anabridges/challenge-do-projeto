aa9d5affb5328fd825c2d97031f227976dd2f95e
Ana Paula <anapaulabridges@icloud.com>
2025-02-17 23:45:24 -0300

    Criando HTML, primeira função e lista gitignore

---
+++ app.js
@@ -0,0 +1,6 @@
+let tentativas = []; // Inicializa um array vazio para armazenar os nomes
+
+function adicionarAmigo(nome) {
+    tentativas.push(nome); // Adiciona o nome ao array tentativas
+    atualizarListaAmigos(); // Atualiza a lista exibida
+}
